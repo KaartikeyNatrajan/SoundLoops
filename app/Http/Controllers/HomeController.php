c7181dbd2382347ddb2fb7a603c9ba8f23aaa32d
Abhinav Bharadwaj <abhinavrb94@gmail.com>
2016-11-11 06:01:41 +0530

    Some stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        Auth::login(\App\User::find(1));
+        // Auth::login(\App\User::find(1));
         $this->middleware('auth', ['except' => 'getLibrary']);
     }
 
